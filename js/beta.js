f119e9a9c0f1f5bfed3c1d4e94101fe6b7981933
Glitch (www-xstarwake-org) <none>
2022-05-12 12:00:43 +0000

    💨💶 Checkpoint ./beta/index.html:41330950/20 ./js/beta.js:41330950/7

--- js/beta.js
+++ js/beta.js
@@ -3,5 +3,5 @@ function xstarwake_chat() {
     xstarwake_iframe.src = "https://www.twitch.tv/embed/xstarwake/chat?parent=www.xstarwake.org"
     xstarwake_iframe.width = "340"
     xstarwake_iframe.height = "400"
-    xstarwake_iframe.style.display = 'inline-block';
+    xstarwake_iframe.style.display = 'block';
 }
