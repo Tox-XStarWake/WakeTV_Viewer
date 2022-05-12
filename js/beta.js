79b9cf04b46a7409a58cb61e6d49eff195722779
Glitch (www-xstarwake-org) <none>
2022-05-12 12:03:08 +0000

    Import of Tox-XStarWake/WakeTV

--- js/beta.js
+++ js/beta.js
@@ -1,7 +1,8 @@
 function xstarwake_chat() {
     var xstarwake_iframe = document.createElement('xstarwake-chat-embed');
     xstarwake_iframe.src = "https://www.twitch.tv/embed/xstarwake/chat?parent=www.xstarwake.org"
+    document.body.appendChild(chat-window);
     xstarwake_iframe.width = "340"
     xstarwake_iframe.height = "400"
-    xstarwake_iframe.style.display = 'block';
+    xstarwake_iframe.style.display = 'inline-block';
 }
